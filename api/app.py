adecc4db069c8af99bf29b593333a4da07fd8496
jmorofsky <jmorofsky@gmail.com>
2026-01-19 00:54:10 -0500

    update route

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.post("/summarize")
+@app.post("/api/summarize")
 def summarize():
     if not request.json:
         return {error: "No input json received."}
